fd28f34cbd60eb2cd4622ca5ca57a99a60761ff8
Mgr. Vladimir Magyar <magyarvladimir@gmail.com>
2017-07-19 09:48:50 +0200

    new tests

--- test/test.go
+++ test/test.go
@@ -48,6 +48,64 @@ func NewTestElem() *TestElem {
 	return elem
 }
 
+//testing constructors
+
+//TestA ...
+type TestA struct {
+	golymer.Element
+}
+
+//Testb ...
+type Testb struct {
+	golymer.Element
+}
+
+type testC struct{}
+type testD struct {
+	testC
+}
+
+func mehA() *TestA { return new(TestA) }
+func mehB() *Testb { return new(Testb) }
+func mehC() *testC { return new(testC) }
+func mehD() *testD { return new(testD) }
+
+//TestConstructorFunction tests if the Define function takes only valid constructors
+func TestConstructorFunction(t *testing.T) {
+	err := golymer.Define(mehA)
+	if err != nil {
+		t.Error(err)
+	}
+	err = golymer.Define(err)
+	if err == nil {
+		t.Errorf("cannot define something that isn't a function")
+	}
+	err = golymer.Define(mehB)
+	if err == nil {
+		t.Errorf("cannot define struct with camelCase name that doesn't have min two bumbs")
+	}
+	err = golymer.Define(mehC)
+	if err == nil {
+		t.Error("cannot define struct that doesn't embedd golymer.Element")
+	}
+	err = golymer.Define(mehD)
+	if err == nil {
+		t.Error("cannot define struct that doesn't embedd golymer.Element")
+	}
+}
+
+//TestTypeAssertion tests if the DOM node (*js.Object) can be type-asserted to own elem type
+func TestTypeAssertion(t *testing.T) {
+	_, ok := js.Global.Get("document").Call("querySelector", "test-elem").Interface().(golymer.CustomElement)
+	if !ok {
+		t.Fatalf("DOM node cannot be type-asserted to CustomElement interface")
+	}
+	_, ok = js.Global.Get("document").Call("querySelector", "test-elem").Interface().(*TestElem)
+	if !ok {
+		t.Fatalf("DOM node cannot be type-asserted to *TestElem")
+	}
+}
+
 //TestDataBindings tests data bindings on the TestElem
 func TestDataBindings(t *testing.T) {
 	testElem := js.Global.Get("document").Call("querySelector", "test-elem").Interface().(*TestElem)
@@ -55,6 +113,16 @@ func TestDataBindings(t *testing.T) {
 		testElem.BackgroundColor = "yellow"
 		if testElem.Children["meh"].Get("style").Get("backgroundColor").String() != "yellow" {
 			t.Error("Error: background-color not set to yellow")
+		}
+		if testElem.Get("background-color").String() != "yellow" {
+			t.Error("Error: background-color attribute of test-elem not set to yellow (it's an exported field, it must set attributes of the tag)")
+		}
+		testElem.Call("setAttribute", "background-color", "green")
+		if testElem.Children["meh"].Get("style").Get("backgroundColor").String() != "green" {
+			t.Error("Error: setting background-color attribute of test-elem didn't set to #meh.style.background-color to green")
+		}
+		if testElem.BackgroundColor != "green" {
+			t.Error("testElem.BackgroundColor is not set to green after setting the attribute background-color of test-elem")
 		}
 	})
 	t.Run("content", func(t *testing.T) {
@@ -80,10 +148,16 @@ func TestDataBindings(t *testing.T) {
 func test() {
 	//flag.Set("test.v", "true")
 	testing.Main(func(pat, str string) (bool, error) { return true, nil },
-		[]testing.InternalTest{{
-			Name: "TestDataBindings",
-			F:    TestDataBindings,
-		}},
+		[]testing.InternalTest{
+			{
+				Name: "TestDataBindings",
+				F:    TestDataBindings,
+			},
+			{
+				Name: "TestTypeAssertion",
+				F:    TestTypeAssertion,
+			},
+		},
 		[]testing.InternalBenchmark{},
 		[]testing.InternalExample{},
 	)
